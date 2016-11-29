971e08320cddb4c65e239af3583e5067d0ca7356
Siennajack <jacopo.delpiano@gmail.com>
2016-11-29 19:19:45 +0000

    Added correction nome

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
 	default to: 'jacopo.delpiano@gmail.com'
 
-	def contact_email(nome, email, body)
+	def contact_email(name, email, body)
 		@name = name
 		@email = email
 		@body = body
